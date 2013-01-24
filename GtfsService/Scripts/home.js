13bdb51670758faa645b04bf6edcde6e1de2586b
Joar Gullestad Pettersen <joargp@gmail.com>
2013-01-24 08:25:44 +0100

    html-stripping

---
+++ GtfsService/Scripts/home.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+    $('home').remove();
+    $('div[class=toolbar]').remove();
+    $('img[id=logo]').remove();
+    $('h2').remove();
+    $("#somevalue").focus();
+    $('p[class=bookmark]').remove();
+});
+
